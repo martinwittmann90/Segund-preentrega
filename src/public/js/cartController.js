9ba1cfb72fe3fe3839c3f8b47e65a8c8496dec6b
Martin Wittmann <107895957+martinwittmann90@users.noreply.github.com>
2023-06-13 00:52:41 -0300

    siguiendo

--- src/public/js/cartController.js
+++ src/public/js/cartController.js
@@ -1,60 +1,55 @@
+//@ts-check
 async function addToCart(productId) {
   try {
-    let cartId = localStorage.getItem('cartId')
-    if (!cartId) {
+    await client.connect();
+    const db = client.db('carts'); //
+
+    let cartCollection = db.collection('carts');
+    let cart = await cartCollection.findOne();
+    
+    if (!cart) {
       const response = await fetch('http://localhost:8080/api/carts', {
         method: 'POST'
-      })
-      const data = await response.json()
-      cartId = data.payload._id
-      localStorage.setItem('cartId', cartId)
+      });
+      const data = await response.json();
+      cart = { _id: data.payload._id };
+      await cartCollection.insertOne(cart);
     }
-    await fetch(`http://localhost:8080/api/carts/${cartId}/product/${productId}`, { method: 'POST' })
-    alert('product added correctly')
+
+    await fetch(`http://localhost:8080/api/carts/${cart._id}/product/${productId}`, { method: 'POST' });
+    alert('product added correctly');
   } catch (error) {
-    alert('failed to add to cart')
+    alert('failed to add to cart');
+  } finally {
+    await client.close();
   }
 }
 
 async function updateCartFunction() {
   try {
-    const cartId = localStorage.getItem('cartId')
-    if (!cartId) {
-      document.getElementById('CartVisuals').textContent = ''
-      return
+    await client.connect();
+    const db = client.db('carts'); // Reemplaza 'tu-base-de-datos' con el nombre de tu base de datos en MongoDB
+
+    let cartCollection = db.collection('carts');
+    let cart = await cartCollection.findOne();
+
+    if (!cart) {
+      document.getElementById('CartVisuals').textContent = '';
+      return;
     }
 
-    const response = await fetch(`http://localhost:8080/api/carts/${cartId}`)
-    const data = await response.json()
+    const response = await fetch(`http://localhost:8080/api/carts/${cart._id}`);
+    const data = await response.json();
 
     if (data.payload && data.payload[0] && data.payload[0].products) {
-      const cart = data.payload[0]
-      const itemCount = cart.products.length
-      document.getElementById('CartVisuals').textContent = itemCount.toString()
+      const itemCount = data.payload[0].products.length;
+      document.getElementById('CartVisuals').textContent = itemCount.toString();
     } else {
-      document.getElementById('CartVisuals').textContent = ''
+      document.getElementById('CartVisuals').textContent = '';
     }
   } catch (error) {
-    document.getElementById('CartVisuals').textContent = ''
+    document.getElementById('CartVisuals').textContent = '';
+  } finally {
+    await client.close();
   }
 }
-try {
-  socket.on('productAdded', async (product) => {
-    const container = document.getElementById('product-container')
-    const productListElement = document.createElement('div')
-    productListElement.innerHTML = `
-              <div id="${product._id}" class="product">
-                <h2>${product.title}</h2>
-                <img src="${product.thumbnails}" alt="">
-                <p><strong>Price:</strong> $${product.price}</p>
-                <p><strong>Stock:</strong> ${product.stock}</p>
-                <button class="delete-btn" onclick="deleteProductWithSocket('${product._id}')">Delete</button>
-              </div>`
-
-    container.appendChild(productListElement)
-  })
-  socket.on('product:deleted', (id) => {
-    const div = document.getElementById(id)
-    div.remove()
-  })
-} catch (error) { }
