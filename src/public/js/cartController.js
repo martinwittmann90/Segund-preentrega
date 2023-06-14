50db20bf22ebf2e4de49de239e8a1d62f0588f65
Martin Wittmann <107895957+martinwittmann90@users.noreply.github.com>
2023-06-14 19:34:20 -0300

    siguiendo

--- src/public/js/cartController.js
+++ src/public/js/cartController.js
@@ -1,21 +1,82 @@
-function addToCart(productId) {
-  const cartId = '6477d7a56ad0a9fc9091dded'; 
-  fetch(`/api/carts/${cartId}/products/${productId}`, {
-    method: 'POST',
+let carritoId = localStorage.getItem("carrito-id");
+const API_URL = "http://localhost:8080/api";
+function putIntoCart(_id) {
+  carritoId = localStorage.getItem("carrito-id");
+  const url = API_URL + "/carts/" + carritoId + "/product/" + _id;
+
+  const data = {};
+
+  const options = {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({
+    body: JSON.stringify(data),
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res);
+      alert("agregado!!!");
     })
-  })
-  .then(response => {
-    if (response.ok) {
-      console.log('Product added to cart');
-    } else {
-      throw new Error('Failed to add product to cart');
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    .catch((error) => {
+      console.error("Error:", error);
+      alert(JSON.stringify(error));
+    });
 }
+
+if (!carritoId) {
+  alert("no id");
+  const url = API_URL + "/carts";
+
+  const data = {};
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Response:", data);
+      const carritoId = localStorage.setItem("carrito-id", data._id);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert(JSON.stringify(error));
+    });
+}
+
+/* 
+
+const carritoID = localStorage.getItem("carrito-id");
+if(!carritoID){
+  alert("no id");
+const url = "http//localhost:8080/api/carts";
+const data = {};
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body:JSON.stringify(data),
+};
+fetch(url,options)
+  .then((response) => response.json())
+  .then((data) => {
+console.log("Response:", data);
+alert(JSON.stringify(data));
+})
+.catch((error)=>{
+  console.log("Error:", error);
+  alert(JSON.stringify(error));
+})
+} else {
+alert(carritoID);
+}
+ */
