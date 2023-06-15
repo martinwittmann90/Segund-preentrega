fcc0134da191e9a41ac2e14ec4cf9b7facb8f37e
Martin Wittmann <107895957+martinwittmann90@users.noreply.github.com>
2023-06-15 17:38:19 -0300

    siguiendo

--- src/DAO/models/product.model.js
+++ src/DAO/models/product.model.js
@@ -5,7 +5,7 @@ const productschema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true, index:true },
   price: { type: Number, required: true },
-  thumbnail: {type: [String], required: true, },
+  thumbnail: {type: String, required: true, },
   code: { type: String, required: true, unique: true },
   stock: { type: Number, required: true },
   category: { type: String, enum: ["Camisetas", "Shorts", "Medias", "Botines", "Camperas"], default: "Camisetas"},
